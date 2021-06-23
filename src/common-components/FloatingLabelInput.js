b7e9d73a80f6ab6202dbe67acdbf5777557f0c03
Vidhya <vidhya.inboxme@gmail.com>
2021-06-23 15:16:03 +0530

    login page UI

--- src/common-components/FloatingLabelInput.js
+++ src/common-components/FloatingLabelInput.js
@@ -6,53 +6,62 @@ import {
   TextInput
 } from "react-native";
 
-export default function FloatingLabelInput() {
+export default function FloatingLabelInput(props) {
+  const { label } = props;
   const [ isFocused, setIsFocused ] = useState(false);
 
   const handleFocus = () => {
     setIsFocused(true);
   }
   return (
-    <View style={styles.BoxWrapper}>
-      <TextInput 
-        style={styles.TextInput} 
-        onFocus={handleFocus}/>
-      <Text 
-        style={isFocused ? styles.TextWrapperFocussed : styles.TextWrapper}
-        onClick={() => {
-          if (!isFocused) {
-            handleFocus()
-          }
-        }}>
-        Enter your email  
-      </Text>
-    </View>
+      <View style={styles.ViewWrapper}>
+        <TextInput 
+          style={styles.TextInput} 
+          onFocus={handleFocus}/>
+        <Text 
+          style={isFocused ? styles.TextWrapperFocussed : styles.TextWrapper}
+          onClick={() => {
+            if (!isFocused) {
+              handleFocus()
+            }
+          }}>
+          {label} 
+        </Text>
+      </View>
   );
 }
 const styles = StyleSheet.create({
+  ViewWrapper: {
+    position: "relative",
+    marginBottom: 20
+  },
   TextInput: {
-    border: "1px solid red",
-    padding: 15,
+    border: "1px solid #1E57F1",
     borderRadius: 4,
-    width: "200px"
+    width: "100%",
+    height: 50,
+    outline: "none",
+    paddingLeft: 20,
+    paddingRight: 20,
+    fontSize: 17
   },
   TextWrapper: {
     fontSize: 16,
     position: "absolute",
-    bottom: 15,
-    color: "red",
+    bottom: 16,
+    color: "#1E57F1",
     left: 14,
     padding: "0 8px",
-    backgroundColor: "#e7e7f8"
+    backgroundColor: "#fff"
   },
   TextWrapperFocussed: {
     fontSize: 16,
     position: "absolute",
-    bottom: 37,
-    color: "red",
+    bottom: 38,
+    color: "#1E57F1",
     left: 14,
     padding: "0 8px",
-    backgroundColor: "#e7e7f8"
+    backgroundColor: "#fff"
   }
 });
 
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { inject, observer } from "mobx-react";
-import { Button } from 'react-native';
+import { ImageBackground, StyleSheet, Image, Text, Button, View } from "react-native";
 
 import FloatingLabelInput from "../common-components/FloatingLabelInput";
 
@@ -9,17 +9,44 @@ const Login = (props) => {
   const { navigation } = props;
 
   return (
-    <>
-      <FloatingLabelInput />
+    <View style={styles.ViewWrapper}>
+      <View style={styles.ViewWrapper2}>
+        <Image style={styles.Logo} source={require("../assets/logo.jpg")} />
 
-
+        <FloatingLabelInput label="Email" />
+        <FloatingLabelInput label="Password" />
+      </View>
       <Button
+          title="Login" />
+
+
+
+      {/* <Button
           title="check posts"
           onPress={() => navigation.navigate('Posts')}
-        />
-    </>
+        /> */}
+    </View>
   );
 };
 
+const styles = StyleSheet.create({
+  ViewWrapper: {
+    height: "100%",
+    backgroundColor: "#fff"
+  },
+  ViewWrapper2: {
+    width: "80%",
+    display: "block",
+    margin: "auto"
+
+  },
+  Logo: {
+    width: 85,
+    height: 85,
+    display: "block",
+    margin: "auto",
+    marginBottom: 20,
+  },
+});
 
 export default inject("store")(observer(Login));
