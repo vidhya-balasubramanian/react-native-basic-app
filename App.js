7430fb6f984179e63c215883b2037a19a0b12d18
Vidhya <vidhya.inboxme@gmail.com>
2021-06-24 11:26:31 +0530

    file name changes

--- App.js
+++ App.js
@@ -4,29 +4,31 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Provider } from 'mobx-react';
 
-import Login from "./src/screens/Login";
+import ConfirmationScreen from "./src/screens/ConfirmationScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import PostsScreen from "./src/screens/PostsScreen";
+import SignupScreen from "./src/screens/SignupScreen";
 import SplashScreen from "./src/screens/SplashScreen";
-import Posts from "./src/screens/Posts";
-
 
 import store from './src/store';
 
 const Stack = createStackNavigator();
-
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName="Splash"
+          initialRouteName="SplashScreen"
           screenOptions={{
             headerShown: false,
           }}
         >
-          <Stack.Screen name="Splash" component={SplashScreen} />
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Posts" component={Posts} />
+          <Stack.Screen name="ConfirmationScreen" component={ConfirmationScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="PostsScreen" component={PostsScreen} />
+          <Stack.Screen name="SignupScreen" component={SignupScreen} />
+          <Stack.Screen name="SplashScreen" component={SplashScreen} />
 
         </Stack.Navigator>
       </NavigationContainer>
